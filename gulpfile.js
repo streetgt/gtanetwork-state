bdca6e6c4591f50bfa57f1fb702ed3f7343f4552
Tiago Cardoso <tiagocardosoweb@gmail.com>
2016-10-16 17:13:37 +0100

    Changed gulpfile JS order

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.sass('app.scss')
     mix.scripts([
+        '../../../node_modules/jquery/dist/jquery.js',
         '../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
-        '../../../node_modules/jquery/dist/jquery.js'
     ], 'public/js/app.js');
 });
