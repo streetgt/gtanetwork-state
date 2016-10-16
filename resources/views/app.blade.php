f1baaeb75641732e82bba0896eb34fe3e21b0551
Tiago Cardoso <tiagocardosoweb@gmail.com>
2016-10-16 17:17:36 +0100

    Changed description / keywords

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="gtanetwork, game state, server info, multiplayer, server, information">
+    <meta name="description" content="GTA Network server monitoring, stats, banner and live signatures!">
+    <meta name="keywords" content="gtanetwork, game state, server info, multiplayer, server, information">
     <meta name="author" content="Tiago Cardoso">
     <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon">
 
