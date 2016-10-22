429b0b41362cc3e2b5d65a3fcdf60c2f8c7c2911
Tiago Cardoso <tiagocardosoweb@gmail.com>
2016-10-22 16:29:20 +0100

    Changed footer copyright name

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
     <div class="copyrights text-right">
-        <p>Tiago Cardoso &copy; 2016</p>
+        <p><a href="https://forum.gtanet.work/index.php?members/streetgt.81/">StreetGT &copy; 2016</a></p>
     </div>
 </footer>
