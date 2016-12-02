d954adf103b6b5ca0f9d9b2eff34a20e6096418c
Tiago Cardoso <tiagocardosoweb@gmail.com>
2016-12-02 20:12:23 +0000

    Added November forum stats

--- resources/views/pages/forum-stats.blade.php
+++ resources/views/pages/forum-stats.blade.php
@@ -12,15 +12,22 @@
         <p class="lead network"><b>Forum Stats - October</b></p>
         <hr>
         <div class="chart">
-            <canvas id="forum_stats_chart" width="400" height="200"></canvas>
+            <canvas id="forum_stats_chart_october" width="400" height="200"></canvas>
         </div>
+        <hr>
+        <p class="lead network"><b>Forum Stats - November</b></p>
+        <hr>
+        <div class="chart">
+            <canvas id="forum_stats_november" width="400" height="200"></canvas>
+        </div>
+
     </div>
 @endsection
 
 @section('javascript')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.3.0/Chart.js"></script>
     <script>
-        var ctx = document.getElementById("forum_stats_chart");
+        var ctx = document.getElementById("forum_stats_chart_october");
         var myChart = new Chart(ctx, {
             type: 'line',
             data: {
@@ -93,6 +100,96 @@
                         557,
                         560,
                         566
+                    ],
+                    backgroundColor: "rgba(0,0,0,0)",
+                    borderColor: "#6f99bb",
+                }]
+            },
+            options: {
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'Forum Users - October'
+                },
+                scales: {
+                    yAxes: [{
+                        display: true,
+                    }]
+                }
+            }
+        });
+
+        var ctx = document.getElementById("forum_stats_chart_november");
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [
+                    "Tue Nov 01 2016",
+                    "Wed Nov 02 2016",
+                    "Thu Nov 03 2016",
+                    "Fri Nov 04 2016",
+                    "Sat Nov 05 2016",
+                    "Sun Nov 06 2016",
+                    "Mon Nov 07 2016",
+                    "Tue Nov 08 2016",
+                    "Wed Nov 09 2016",
+                    "Thu Nov 10 2016",
+                    "Fri Nov 11 2016",
+                    "Sat Nov 12 2016",
+                    "Sun Nov 13 2016",
+                    "Mon Nov 14 2016",
+                    "Tue Nov 15 2016",
+                    "Wed Nov 16 2016",
+                    "Thu Nov 17 2016",
+                    "Fri Nov 18 2016",
+                    "Sat Nov 19 2016",
+                    "Sun Nov 20 2016",
+                    "Mon Nov 21 2016",
+                    "Tue Nov 22 2016",
+                    "Wed Nov 23 2016",
+                    "Thu Nov 24 2016",
+                    "Fri Nov 25 2016",
+                    "Sat Nov 26 2016",
+                    "Sun Nov 27 2016",
+                    "Mon Nov 28 2016",
+                    "Tue Nov 29 2016",
+                    "Wed Nov 30 2016",
+                    "Thu Dec 01 2016"
+                ],
+                datasets: [{
+                    label: 'Users',
+                    data: [
+                        566,
+                        572,
+                        578,
+                        583,
+                        587,
+                        590,
+                        590,
+                        594,
+                        596,
+                        600,
+                        603,
+                        606,
+                        608,
+                        614,
+                        618,
+                        620,
+                        621,
+                        625,
+                        627,
+                        631,
+                        634,
+                        638,
+                        643,
+                        646,
+                        648,
+                        651,
+                        652,
+                        657,
+                        658,
+                        658,
+                        660
                     ],
                     backgroundColor: "rgba(0,0,0,0)",
                     borderColor: "#6f99bb",
