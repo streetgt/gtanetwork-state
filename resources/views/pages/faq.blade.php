8a018044116c3625cbb4ffdc73890a63b376ecb2
Tiago Cardoso <tiagocardosoweb@gmail.com>
2016-12-16 20:24:58 +0000

    - Changed database configuration - Added donation link

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -29,6 +29,16 @@
                         <li>You can send me a private message in <a href="https://forum.gtanet.work/index.php?members/streetgt.81/">GTA Network Forum</a></li>
                     </ul>
                 </li>
+
+                <hr>
+
+                <li>
+                    <b>Looking for send me a donation?</b>
+                    <ul>
+                        <li>Currently I support the website from my own pocket, hosted on DigitalOcean $5/month,
+                            If you really enjoy the platform and want to contribute and help me running the platform, you can donate <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=tiagocardosoweb%40gmail%2ecom&lc=PT&item_name=GTA%20NETWORK%20%2d%20SERVER%20STATE&item_number=gtanetworkserverstate&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted">here</a>.</li>
+                    </ul>
+                </li>
             </ul>
 
         </div>
--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
     <div class="copyrights text-right">
-        <p><a href="https://forum.gtanet.work/index.php?members/streetgt.81/">StreetGT &copy; 2016</a> v0.11</p>
+        <p><a href="https://forum.gtanet.work/index.php?members/streetgt.81/">StreetGT &copy; 2016</a> v0.12</p>
     </div>
 </footer>
