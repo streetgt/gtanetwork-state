2023c82a4dc81c709aae7628e1f6dff29c44ae84
Tiago Cardoso <tiagocardosoweb@gmail.com>
2016-12-17 01:55:56 +0000

    -Added 'v' in version

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -2,6 +2,6 @@
 
 <footer class="footer">
     <div class="copyrights text-right">
-        <p><a href="https://forum.gtanet.work/index.php?members/streetgt.81/">StreetGT &copy; 2016</a>  {{ formatVersion($service->getTotalCommits()) }}</p>
+        <p><a href="https://forum.gtanet.work/index.php?members/streetgt.81/">StreetGT &copy; 2016</a>  v{{ formatVersion($service->getTotalCommits()) }}</p>
     </div>
 </footer>
